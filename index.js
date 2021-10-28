15564717d59c5423369d582f5f6d0018d546ca90
Alamin <alamin.mj.3228@gmail.com>
2021-10-29 00:30:23 +0600

    created server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.begpz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    app.post("/", async (req, res) => {
+      res.send("ok vai post");
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Ok vai send");
+});
+app.listen(port, () => {
+  console.log(`listeing on ${port}`);
+});
